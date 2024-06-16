850b47ed901ef8ed174c3c8bac94d6fdf5209808
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2024-06-16 13:00:42 +0100

    initial commit

---
+++ src/main/java/io/undefvar/text_classification_service/TextClassificationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.undefvar.text_classification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextClassificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextClassificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/undefvar/text_classification_service/TextClassificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.undefvar.text_classification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextClassificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
